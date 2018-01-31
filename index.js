d74803d76dd458f12da537bff0ca066bb4533735
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-31 00:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function setThisWithApply(fn, thisValue, args) {
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
-  return thisValue.bind(this)
+  return functionToBeCopied.bind(thisValue)
 }
